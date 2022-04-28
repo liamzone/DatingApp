a2ebace02b97cc1b2006a40ac305ed98ecfd4e56
Liam O'Neill <contact.liamzone@gmail.com>
2022-04-28 13:22:14 +0200

    Updated Registration and Form Inputs

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -9,16 +9,17 @@ using AutoMapper;
 
 namespace API.Helpers
 {
-    public class AutoMapperProfiles : Profile
+  public class AutoMapperProfiles : Profile
+  {
+    public AutoMapperProfiles()
     {
-        public AutoMapperProfiles()
-        {
-            CreateMap<AppUser, MemberDto>()
-                .ForMember(dest => dest.PhotoUrl, opt => opt.MapFrom(src =>
-                    src.Photos.FirstOrDefault(x => x.IsMain).Url))
-                .ForMember(dest => dest.Age, opt => opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
-            CreateMap<Photo, PhotoDto>();
-            CreateMap<MemberUpdateDto, AppUser>();
-        }
+      CreateMap<AppUser, MemberDto>()
+          .ForMember(dest => dest.PhotoUrl, opt => opt.MapFrom(src =>
+              src.Photos.FirstOrDefault(x => x.IsMain).Url))
+          .ForMember(dest => dest.Age, opt => opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
+      CreateMap<Photo, PhotoDto>();
+      CreateMap<MemberUpdateDto, AppUser>();
+      CreateMap<RegisterDto, AppUser>();
     }
+  }
 }
